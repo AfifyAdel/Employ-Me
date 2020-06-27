dd3627b33969e87a3c9590f41df51c8154e1163a
Afify Adel <afify.adel@rosenfielddwc.com>
2020-06-27 21:46:43 +0200

    --Add start up data

--- EmployMe/Startup.cs
+++ EmployMe/Startup.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using WebApplication2.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication2.Startup))]
 namespace WebApplication2
 {
     public partial class Startup
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateDefaultRolesAndUsers();
+        }
+        public void CreateDefaultRolesAndUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            IdentityRole role = new IdentityRole();
+            if (!roleManager.RoleExists("Admins"))
+            {
+                role.Name = "Admins";
+                roleManager.Create(role);
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "Afify";
+                user.Email = "afifyadel25@gmail.com";
+                var check = userManager.Create(user, "Afify123@");
+                if (check.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admins");
+                }
+            }
         }
     }
 }
